082fed97a25c5b69f6e280e3573eb63b74920441
Timo H <timo.herttua@efekta.fi>
2018-02-18 20:14:00 +0200

    osa442

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  const reducer = (sum, item) => {
+    if (item.likes) {
+      return sum + item.likes
+    } else {
+      console.log("Item did not contain the likes attribute: ", item)
+      console.log(blogs)
+      return sum + 0
+    }
+    
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
