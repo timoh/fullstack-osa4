c77461900388acc65287b1a5cd36ba8f6a7651bb
Timo H <timo.herttua@efekta.fi>
2018-02-19 22:00:53 +0200

    osa492

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -21,9 +21,14 @@ Format of a post:
 
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-
   const result = await blog.save()
   response.status(201).json(result)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  await Blog.findByIdAndRemove(id)
+  response.status(201).json(id)
 })
 
 module.exports = blogRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,28 +2,62 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('GET /api/blogs', () => {
 
-test('there are three blogs', async () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are three blogs', async () => {
     const response = await api
       .get('/api/blogs')
-  
+
     expect(response.body.length).toBe(3)
   })
-  
+
   test('the first blog is interesting', async () => {
     const response = await api
       .get('/api/blogs')
-    
-      // console.log("Response body:",response.body)
+
+    // console.log("Response body:",response.body)
 
     expect(response.body[0].title).toBe('Interesting topic')
   })
+
+})
+
+describe('POST /api/blogs', () => {
+
+  test('POSTing a new blog posts results in more posts', async () => {
+    const newPost = {
+      "author": "Dean",
+      "title": "Much More interesting topic",
+      "url": "/api/blogs",
+      "likes": 5
+    }
+
+    const responseOne = await api
+      .post('/api/blogs')
+      .send(newPost)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const newId = responseOne.body._id
+
+    await api.delete(`/api/blogs/${newId}`)
+
+    const responseTwo = await api
+    .get('/api/blogs')
+
+    expect(responseTwo.body.length).toBe(3)
+  })
+
+})
+
+
 
 afterAll(() => {
   server.close()
