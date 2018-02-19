08cfc0c5204f941e706c871e9f0d811fab28649a
Timo H <timo.herttua@efekta.fi>
2018-02-19 21:35:34 +0200

    osa481-asyncawait

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,9 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 /*
@@ -22,14 +19,11 @@ Format of a post:
 
 */
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogRouter
